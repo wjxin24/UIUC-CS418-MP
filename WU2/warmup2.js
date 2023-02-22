3b6cd51c21eb92479e21796e368d0baa5c27c72e
Wu Jiaxin <lisa24@connect.hku.hk>
2023-02-22 12:12:09 -0600

    warmup 2

---
+++ WU2/warmup2.js
@@ -0,0 +1,50 @@
+function compileAndLinkGLSL(vs_source, fs_source) { 
+  let vs = gl.createShader(gl.VERTEX_SHADER)
+  gl.shaderSource(vs, vs_source)
+  gl.compileShader(vs)
+  if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+      console.error(gl.getShaderInfoLog(vs))
+      throw Error("Vertex shader compilation failed")
+  }
+
+  let fs = gl.createShader(gl.FRAGMENT_SHADER)
+  gl.shaderSource(fs, fs_source)
+  gl.compileShader(fs)
+  if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+      console.error(gl.getShaderInfoLog(fs))
+      throw Error("Fragment shader compilation failed")
+  }
+
+  window.program = gl.createProgram()
+  gl.attachShader(program, vs)
+  gl.attachShader(program, fs)
+  gl.linkProgram(program)
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.error(gl.getProgramInfoLog(program))
+      throw Error("Linking failed")
+  }
+}
+
+function draw(milliseconds) {
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.useProgram(program)
+    let secondsBindPoint = gl.getUniformLocation(program, 'seconds')
+    gl.uniform1f(secondsBindPoint, milliseconds/1000)
+    const connection = gl.POINTS
+    const offset = 0                          // unused here, but required
+    const count = 6+(0|milliseconds/100)%100  // number of vertices to draw
+    let countBindPoint = gl.getUniformLocation(program, 'count')
+    gl.uniform1i(countBindPoint, count)
+    gl.drawArrays(connection, offset, count)
+    window.animation = requestAnimationFrame(draw)
+}
+
+async function setup(event) {
+  window.gl = document.querySelector('canvas').getContext('webgl2')
+  let vs = await fetch('vertex.glsl').then(res => res.text())
+  let fs = await fetch('fragment.glsl').then(res => res.text())
+  compileAndLinkGLSL(vs,fs)
+  draw()
+}
+
+window.addEventListener('load',setup)
